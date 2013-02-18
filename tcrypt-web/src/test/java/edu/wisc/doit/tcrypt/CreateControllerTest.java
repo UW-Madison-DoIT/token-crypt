78dbbcad215a6cca6e9fd2a53af95af159580af7
levett <levett@levett-OptiPlex-790.(none)>
2013-02-18 15:45:14 -0600

    Forgot to commit this before.  Changes to class tests

--- tcrypt-web/src/test/java/edu/wisc/doit/tcrypt/CreateControllerTest.java
+++ tcrypt-web/src/test/java/edu/wisc/doit/tcrypt/CreateControllerTest.java
@@ -1,5 +1,7 @@
 package edu.wisc.doit.tcrypt;
 
+import java.security.KeyPair;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.junit.Test;
@@ -11,8 +13,11 @@ import org.springframework.web.servlet.ModelAndView;
 
 import edu.wisc.doit.tcrypt.controller.CreateController;
 import edu.wisc.doit.tcrypt.dao.IKeysKeeper;
+import edu.wisc.doit.tcrypt.exception.ServiceErrorException;
 import edu.wisc.doit.tcrypt.services.TCryptServices;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -27,18 +32,27 @@ public class CreateControllerTest {
 		assertEquals(handleRequest.getViewName(),"createServiceKeyBefore");
 	}
 	
-	/*@Test
+	@Test
 	public void testKeyCreationFailure() throws Exception {
 		when(tcryptServices.generateKeyPair(2048)).thenReturn(null);
-		ModelAndView mav = createController.createServiceKey("test", 2048, request);
-		assertEquals(mav.getViewName(),"createServiceKeyBefore");
-	}*/
+		try {
+			ModelAndView mav = createController.createServiceKey("test", 2048, request);
+			assertNotNull(mav);
+		} catch (Exception e) {
+			if(e instanceof ServiceErrorException) {
+				//expected, do nothing
+			} else {
+				fail();
+			}
+		}
+	}
 	
 	@Test
 	public void testKeyCreationSuccess() throws Exception {
-		
-		//ModelAndView mav = createController.createServiceKey("test", 2048, request);
-		//assertEquals(mav.getViewName(),"createServiceKeyDownload");
+		//TODO Not sure how to test this since Mockito can't mock KeyPair class.
+		/*when(tcryptServices.generateKeyPair(2048)).thenReturn(generatedKeyPair);
+		ModelAndView mav = createController.createServiceKey("test", 2048, request);
+		assertEquals(mav.getViewName(),"createServiceKeyDownload");*/
 	}
 
 }
