358acd5b8b38a4e7802d1a7241302b7c8331545c
Brad Leege <bleege@gmail.com>
2013-02-18 15:34:37 -0600

    [TCRYPT-20] integrating download key api into the ui

--- tcrypt-web/src/main/java/edu/wisc/doit/tcrypt/controller/DownloadController.java
+++ tcrypt-web/src/main/java/edu/wisc/doit/tcrypt/controller/DownloadController.java
@@ -1,51 +1,44 @@
 package edu.wisc.doit.tcrypt.controller;
 
-import java.io.FileWriter;
-import java.security.Key;
-
 import edu.wisc.doit.tcrypt.services.TCryptServices;
 import edu.wisc.doit.tcrypt.vo.ServiceKey;
-
-import org.bouncycastle.openssl.PEMWriter;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.Reader;
+import java.security.Key;
 
 @Controller
-public class DownloadController extends BaseController {
-	
+public class DownloadController extends BaseController
+{
 	private TCryptServices tcryptServices;
-	
+
 	@Autowired
-	public DownloadController(TCryptServices tcryptServices) {
+	public DownloadController(TCryptServices tcryptServices)
+	{
 		this.tcryptServices = tcryptServices;
 	}
-	
-	@RequestMapping("/download")
-	public void downloadKey(@RequestParam("serviceName") String serviceName,@RequestParam("keyType") String keyType, HttpServletRequest request, HttpServletResponse response) throws Exception {
 
-		try {
+	@RequestMapping("/download")
+	public void downloadKey(@RequestParam("serviceName") String serviceName, @RequestParam("keyType") String keyType, HttpServletRequest request, HttpServletResponse response) throws Exception
+	{
+		try
+		{
 			ServiceKey sk = (ServiceKey) request.getSession().getAttribute("serviceKey_" + serviceName);
-			
-		    response.setContentType("text/plain");
-		    response.setHeader("Content-Disposition", "attachment; filename=\"" + keyType + sk.getServiceName() + ".pem" + "\"");
-		    //response.setContentLength(keyAsBytes.length);
-		    //FileCopyUtils.copy(in, response.getWriter());
-			//FileCopyUtils.copy(sk.getPrivateKey().getEncoded(), response.getOutputStream());
-		    
-		    //Once this is fixed in the API replace with above
-		    final PEMWriter pemWriter = new PEMWriter(response.getWriter());
-		    Key myKey = "private".equals(keyType) ? sk.getPrivateKey() : sk.getPublicKey();
-		    pemWriter.writeObject(myKey);
-			pemWriter.close();
-		    
-		} catch (Exception e) {
-			logger.error("Issue downloading the key " + keyType,e);
-			throw new Exception (e);
+			response.setContentType("application/x-pem-file");
+			response.setHeader("Content-Disposition", "attachment; filename=\"" + keyType + "-" + sk.getServiceName() + ".pem" + "\"");
+			Key key = "private".equalsIgnoreCase(keyType) ? sk.getPrivateKey() : sk.getPublicKey();
+			Reader keyReader = tcryptServices.getKeyAsInputStreamReader(key);
+			IOUtils.copy(keyReader, response.getOutputStream());
+		}
+		catch (Exception e)
+		{
+			logger.error("Issue downloading the key " + keyType, e);
+			throw new Exception(e);
 		}
 	}
 }
