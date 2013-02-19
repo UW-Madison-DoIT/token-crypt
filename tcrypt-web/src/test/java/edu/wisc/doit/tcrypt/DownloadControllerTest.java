d0e93e065ab98c323e5d5a55b4d0cf4c039cf5cb
Brad Leege <bleege@gmail.com>
2013-02-19 11:41:16 -0600

    [TCRYPT-20] adding basic unit test for DownloadController that insures data in equals data out

--- tcrypt-web/src/test/java/edu/wisc/doit/tcrypt/DownloadControllerTest.java
+++ tcrypt-web/src/test/java/edu/wisc/doit/tcrypt/DownloadControllerTest.java
@@ -1,26 +1,48 @@
 package edu.wisc.doit.tcrypt;
 
 import edu.wisc.doit.tcrypt.controller.DownloadController;
-import edu.wisc.doit.tcrypt.dao.IKeysKeeper;
+import edu.wisc.doit.tcrypt.services.TCryptServices;
+import edu.wisc.doit.tcrypt.vo.ServiceKey;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
-import static org.junit.Assert.assertTrue;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import java.io.Reader;
+import java.io.StringReader;
+import java.security.PublicKey;
+import static junit.framework.Assert.assertEquals;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
-public class DownloadControllerTest {
-	@InjectMocks private DownloadController downloadController;
-	@Mock private IKeysKeeper tcryptHelper;
-	
+public class DownloadControllerTest
+{
+	@InjectMocks
+	private DownloadController downloadController;
+	@Mock
+	private TCryptServices tCryptServices;
+	@Mock
+	private ServiceKey serviceKey;
+	@Mock
+	private PublicKey publicKey;
+	private MockHttpServletRequest request;
+	private MockHttpServletResponse response;
+	private Reader publicKeyReader;
+
 	@Test
-	public void testDownloadAKey () throws Exception {
-/*
-		when(tcryptHelper.getKeyLocationToDownloadFromServer("test","test","private")).thenReturn("test");
-		//TODO : Adjust test case when API is in place.
-*/
-		//downloadController.downloadKey("test", "test", Mockito.mock(HttpServletRequest.class), Mockito.mock(HttpServletResponse.class));
-		assertTrue(true);
+	public void testDownloadAKey() throws Exception
+	{
+		request = new MockHttpServletRequest();
+		response = new MockHttpServletResponse();
+		request.getSession().setAttribute("serviceKey_testkey", serviceKey);
+		publicKeyReader = new StringReader("foo");
+		when(serviceKey.getPublicKey()).thenReturn(publicKey);
+		when(tCryptServices.getKeyAsInputStreamReader(publicKey)).thenReturn(publicKeyReader);
+
+		downloadController.downloadKey("testkey", "public", request, response);
+		String val = response.getContentAsString();
+		assertEquals(val, "foo");
 	}
 }
